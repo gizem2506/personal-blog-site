8bbb11aec61cf6ec541494e73381db9c827a46a0
Gizem Altay <gizemaltay0625@gmail.com>
2024-05-28 00:28:18 +0300

    .

--- src/components/Works-style4/works-style4.jsx
+++ src/components/Works-style4/works-style4.jsx
@@ -1,20 +1,36 @@
-/* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import initIsotope from "../../common/initIsotope";
 
 const WorksStyle4 = () => {
-  React.useEffect(() => {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    // Fetch GitHub repos data
+    const fetchRepos = async () => {
+      try {
+        const response = await fetch("https://api.github.com/users/gizem2506/repos");
+        const data = await response.json();
+        console.log("first",data)
+        setRepos(data);
+      } catch (error) {
+        console.error("Error fetching repos:", error);
+      }
+    };
+
+    fetchRepos();
+
     setTimeout(() => {
       initIsotope();
     }, 1000);
   }, []);
+
   return (
     <section className="portfolio-frl section-padding pb-70">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-8 col-md-10">
-            <div className="sec-head  text-center">
+            <div className="sec-head text-center">
               <h6 className="wow fadeIn" data-wow-delay=".5s">
                 Portfolio
               </h6>
@@ -27,61 +43,33 @@ const WorksStyle4 = () => {
       </div>
       <div className="container">
         <div className="row">
-
-
           <div className="gallery full-width">
-          
-            <div
-              className="col-md-6 items graphic lg-mr wow fadeInUp"
-              data-wow-delay=".4s"
-            >
-              <div className="item-img">
-                <div className="cont">
-                  <h6>Creative Mobile App</h6>
-                  <p>Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                  </p>
-                </div>
+            {repos.map((repo) => (
+              <div
+                key={repo.id}
+                className="col-md-6 items graphic lg-mr wow fadeInUp"
+                data-wow-delay=".4s"
+              >
+                <div className="item-img">
+                  <div className="cont">
+                    <h6>{repo.name}</h6>
+                    <p>{repo.description}</p>
+                    <p>{repo.language}</p>
+                    <p>{repo.pushed_at}</p>
 
-                <div className="btn-more mt-30">
-                  <Link href="/blog-details/blog-details-dark">
-                  <a className="simple-btn">Github</a>
-                  </Link>
-                </div>
-              </div>
-              
-            </div>
+                    
 
-            <div
-              className="col-md-6 items graphic lg-mr wow fadeInUp"
-              data-wow-delay=".4s"
-            >
-              <div className="item-img">
-                <div className="cont">
-                  <h6>Creative Mobile App</h6>
-                  <p>Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                    Ui / Ux creative mobile app design
-                  </p>
-                </div>
-
-                <div className="btn-more mt-30">
-                  <Link href="/blog-details/blog-details-dark">
-                    <a className="simple-btn">Github</a>
-                  </Link>
+                  </div>
+                  <div className="btn-more mt-30">
+                    <Link href={repo.html_url}>
+                      <a className="simple-btn" target="_blank" rel="noopener noreferrer">
+                        Github
+                      </a>
+                    </Link>
+                  </div>
                 </div>
               </div>
-              
-            </div>
-
-
+            ))}
           </div>
         </div>
       </div>
